d4feb171c8dba6a0e23e4d6eacc6e825e436643a
qy_gopher <yqs_ml@163.com>
2022-07-17 18:23:02 +0800

    集成gorilla/mux路由

--- main.go
+++ main.go
@@ -3,23 +3,58 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
-	switch r.URL.Path {
-	case "/":
-		fmt.Fprint(w, "<h1>hello blogo</h1>")
-	case "/about":
-		fmt.Fprint(w, "<h1>blogo可以用来分享信息</h1>")
-	default:
-		w.WriteHeader(http.StatusNotFound)
-		fmt.Fprintf(w, "<h1>页面未找到: %s</h1>", r.Host+r.URL.Path)
-	}
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+
+	fmt.Fprint(w, "<h1>Hello blogo</h1>")
+}
+
+func aboutHanher(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "test/html; charset=utf-8")
+
+	fmt.Fprint(w, "<h1>blogo可以用来记录和分享信息</h1>")
+}
+
+func notFoundHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+
+	w.WriteHeader(http.StatusNotFound)
+
+	fmt.Fprint(w, "<h1>请求页面未找到</h1>")
+}
+
+func articlesShowHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+
+	fmt.Fprintf(w, "文章ID: %s", id)
+}
+
+func articlesIndexHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "文章列表页")
+}
+
+func articlesStoreHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "创建文章页")
 }
 
 func main() {
-	http.HandleFunc("/", handlerFunc)
-	err := http.ListenAndServe(":9090", nil)
+	router := mux.NewRouter()
+
+	router.HandleFunc("/", homeHandler).Methods("GET").Name("home")
+	router.HandleFunc("/about", aboutHanher).Methods("GET").Name("about")
+
+	router.HandleFunc("/articles/{id:[0-9]+}", articlesShowHandler).Methods("GET").Name("articles.show")
+	router.HandleFunc("/articles", articlesIndexHandler).Methods("GET").Name("articles.index")
+	router.HandleFunc("/articles", articlesStoreHandler).Methods("POST").Name("articles.store")
+
+	router.NotFoundHandler = http.HandlerFunc(notFoundHandler)
+
+	err := http.ListenAndServe(":9090", router)
 	if err != nil {
 		panic(err)
 	}
