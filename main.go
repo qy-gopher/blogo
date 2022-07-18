ff702ee0f7f4410e612fd03dbcd779457cad61e4
qy_gopher <yqs_ml@163.com>
2022-07-18 10:20:01 +0800

    移除URL最后的斜杠，以使请求路径有无斜杠均返回相同内容

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strings"
 
 	"github.com/gorilla/mux"
 )
@@ -46,6 +47,17 @@ func forceHTMLMiddleware(next http.Handler) http.Handler {
 	})
 }
 
+func removeTrailingSlash(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// 移除首页外的请求路径最后的'/'
+		if r.URL.Path != "/" {
+			r.URL.Path = strings.TrimSuffix(r.URL.Path, "/")
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
+
 func main() {
 	router := mux.NewRouter()
 
@@ -60,7 +72,7 @@ func main() {
 
 	router.Use(forceHTMLMiddleware)
 
-	err := http.ListenAndServe(":9090", router)
+	err := http.ListenAndServe(":9090", removeTrailingSlash(router))
 	if err != nil {
 		panic(err)
 	}
